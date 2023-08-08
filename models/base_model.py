8aeb4266852d92f3cf6b5fac239c8ccc40847963
lawrence-254 <lodhiambo26@gmail.com>
2023-08-08 05:30:29 -0700

    kwargs/args

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,20 @@ from datetime import datetime
 class BaseModel():
     """Defining public instance attributes"""
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        """check if kwargs is empty or not"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.fromisoformat(value)
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     """used to print info"""
     def __str__(self):
