f45bd3d1b2a70c63a246d1fa61f966cf39c30bca
lawrence-254 <lodhiambo26@gmail.com>
2023-08-08 23:11:34 -0700

    console

--- console.py
+++ console.py
@@ -6,12 +6,19 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
+
     def do_quit(self, arg):
         """used to quit the program"""
         return True
-    
+
     def do_EOF(self, line):
         """allows end of file function"""
         return True
+
+    def emptyline(self):
+        """prevents the prompt from executing the last command"""
+        pass
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
