f90c68c0df45f7a835c34ed68e6724ff0dc014d1
lawrence-254 <lodhiambo26@gmail.com>
2023-08-12 13:38:34 -0700

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,16 +6,16 @@ from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
 
+
 class TestFileStorageClass(unittest.TestCase):
     """some of the test cases for FileStorage class """
-    
+
     def test_all_returns_dict(self):
         """ Test if the all() method returns a dictionary """
         storage = FileStorage()
         obj_dict = storage.all()
         self.assertIsInstance(obj_dict, dict)
         self.assertIs(obj_dict, storage._FileStorage__objects)
-
 
     def test_new_method(self):
         """
@@ -32,7 +32,7 @@ class TestFileStorageClass(unittest.TestCase):
     def test_private_class_instance(self):
         """
            Test if the __file_path and __objects attributes are
-           initialized correctly whenever a new instance of FileStorage is created
+           initialized whenever a new instance of FileStorage is created
         """
         storage = FileStorage()
         self.assertEqual(storage._FileStorage__file_path, "file.json")
